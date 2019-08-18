6ea8a45603f2f436a2a74b1f06066dccfe4161cf
ubuettemeier <ubuettemeier@web.de>
2019-08-18 11:55:33 +0200

    example matchShapes.cpp optimiert

--- samples/matchShapes/matchshapes.cpp
+++ samples/matchShapes/matchshapes.cpp
@@ -18,13 +18,24 @@ using namespace std;
 void build_image_1 (cv::Mat &image1, uint8_t mode)
 {
     if (mode == 0)                                                                      // Circle
-        cv::circle(image1, cv::Point(200, 200), 80, Scalar( 0 ), 2 );
+        cv::circle(image1, cv::Point(200, 200), 80, cv::Scalar( 0 ), 2 );
     if (mode == 1)                                                                      // Rectangle
-        cv::rectangle(image1, cv::Rect(100, 100, 200, 200), Scalar( 0 ), 2);
+        cv::rectangle(image1, cv::Rect(100, 100, 200, 200), cv::Scalar( 0 ), 2);
     if (mode == 2) {                                                                    // Triangle
-        cv::line (image1, cv::Point(100, 300), cv::Point(200, 100), Scalar( 0 ), 2);
-        cv::line (image1, cv::Point(200, 100), cv::Point(300, 300), Scalar( 0 ), 2);
-        cv::line (image1, cv::Point(300, 300), cv::Point(100, 300), Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(100, 300), cv::Point(200, 100), cv::Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(200, 100), cv::Point(300, 300), cv::Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(300, 300), cv::Point(100, 300), cv::Scalar( 0 ), 2);
+    }
+    if (mode == 3) {                                                                    // cross
+        cv::line (image1, cv::Point(100, 100), cv::Point(300, 300), cv::Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(100, 300), cv::Point(300, 100), cv::Scalar( 0 ), 2);
+    }
+    if (mode == 4) {
+        cv::line (image1, cv::Point(150, 100), cv::Point(150, 300), cv::Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(250, 100), cv::Point(250, 300), cv::Scalar( 0 ), 2);
+
+        cv::line (image1, cv::Point(100, 150), cv::Point(300, 150), cv::Scalar( 0 ), 2);
+        cv::line (image1, cv::Point(100, 250), cv::Point(300, 250), cv::Scalar( 0 ), 2);
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,8 +49,8 @@ void build_image_2 (cv::Mat &image2)
     static bool count_up = true;
 
     cv::ellipse( image2, cv::Point(200, 200),
-                 Size( 5+delta_axis_size, 120-delta_axis_size ),
-                 angle, 0, 360, Scalar( 0 ), 2, 8 );
+                 cv::Size( 5+delta_axis_size, 120-delta_axis_size ),
+                 angle, 0, 360, cv::Scalar( 0 ), 2, 8 );
 
     if (angle < 359.0) angle += 1.0;
         else angle = 0.0;
@@ -71,19 +82,19 @@ int main()
         double runtime = (t1-t0)/cv::getTickFrequency();        // control time
         if (runtime > 2.0) {                                    // 2 seconds
             t0 = cv::getTickCount();
-            mode = (mode < 2) ? mode+1 : 0;
+            mode = (mode < 4) ? mode+1 : 0;
         }
 
-        Mat image1 = Mat(400, 400, CV_8UC1, 255);
-        Mat image2 = Mat(400, 400, CV_8UC1, 255);
+        cv::Mat image1 = cv::Mat(400, 400, CV_8UC1, 255);
+        cv::Mat image2 = cv::Mat(400, 400, CV_8UC1, 255);
 
         build_image_1 (image1, mode);
         build_image_2 (image2);
 
-        double result = CVD::matchShapes(image1, image2, CV_CONTOURS_MATCH_I1, 0);          // match the images
+        double result = CVD::matchShapes(image1, image2, cv::CONTOURS_MATCH_I1, 0);          // match the images
         sprintf (buf, "%1.8f", result);
 
-        putText(image2, buf, cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0), 1, CV_AA);
+        cv::putText(image2, buf, cv::Point(30,30), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, cv::Scalar(0), 1);
 
         cv::imshow ("image_1", image1);
         cv::imshow ("image_2", image2);
